8ed013f6847d3ed56bf1b40e9d4b67cfcaea077c
Gabriela Rodrigues <78517025+gabirrodrigues@users.noreply.github.com>
2023-06-09 13:09:22 -0300

    Update projetoTurismo.py

--- projetoTurismo.py
+++ projetoTurismo.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 from io import StringIO
 import os
+from sqlalchemy import create_engine
 
 # URL base dos arquivos CSV
 base_url = 'https://dados.turismo.gov.br/dataset/chegada-de-turistas-internacionais/resource/'
@@ -26,6 +27,12 @@ folder_path = 'C:/Users/Gabriela/Downloads/Impacta/config/venv'
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
+# Criar conexão com o banco de dados SQL Server
+username = 'impacta'
+password = '1234'
+database = 'dados_turismo'
+engine = create_engine(f'mssql+pyodbc://{username}:{password}@LAPTOP-EI084BHV\GABIIBR/{database}?driver=ODBC+Driver+17+for+SQL+Server')
+
 # Loop para fazer download dos arquivos CSV e concatená-los
 df_concatenado = pd.DataFrame()
 for ano, url in urls.items():
@@ -36,5 +43,11 @@ for ano, url in urls.items():
 # Visualização das primeiras linhas
 print(df_concatenado.head())
 
-# Salvando concatenado em um novo arquivo CSV
+# Concatenado no arquivo CSV
 df_concatenado.to_csv(os.path.join(folder_path, 'chegada_turistas_internacionais.csv'), index=False)
+
+# Concatenado no arquivo XLSX
+df_concatenado.to_excel(os.path.join(folder_path, 'chegada_turistas_internacionais.xlsx'), index=False)
+
+# Salvar DataFrame no SQL Server
+df_concatenado.to_sql('chegada_turistas_internacionais', con=engine, if_exists='replace', index=False)
